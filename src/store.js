4ceb45fd199e330cf43f469324f9b27af9e1b412
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-26 17:02:10 -0600

    Set function that will handle state

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartReducer,
+  },
 });
