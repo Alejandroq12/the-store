7af0afa686b73a02725ce0ec25acd0221e052641
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-26 16:33:28 -0600

    Set createSlice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cartItems: [],
+  amount: 0,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+});
+
+console.log(cartSlice);
