5cae0fb667806917adf9c72d1cb80638ca2cb0b4
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-28 23:58:30 -0600

    Add navigation bar structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,12 @@ import { CartIcon } from '../icons';
 import { useSelector } from 'react-redux';
 
 const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
-}
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>The Store</h3>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
