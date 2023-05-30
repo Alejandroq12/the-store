499bad5c6c26491c1a1b3470a228d6fefa72bc16
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-29 23:22:02 -0600

    Add hardcoded total amount

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,9 @@ const Navbar = () => {
         <h3>The Store</h3>
         <div className="nav-container">
           <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">0</p>
+          </div>
         </div>
       </div>
     </nav>
