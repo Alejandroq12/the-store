8c4d2583c49cfb676aed5f5dc48c1c64841a6780
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-31 00:17:51 -0600

    Add cart items

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
 
 const initialState = {
-  cartItems: [],
+  cartItems: cartItems,
   amount: 0,
   total: 0,
   isLoading: true,
