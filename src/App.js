1f04e9924dde8c57d23473c8ae61b77a21d9ce5c
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-31 01:17:59 -0600

    Add container to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+
 function App() {
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
