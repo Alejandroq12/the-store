b88f22de08cab4e9fa017a94e5775883fc21dcf3
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-01 23:53:09 -0600

    Add condition to check if cart is empty

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 
 const CartContainer = () => {
   const { cartItems, total, amount } = useSelector((store) => store.cart);
+  if (amount < 1) {
+
   return <div>CartContainer</div>;
 };
 export default CartContainer;
