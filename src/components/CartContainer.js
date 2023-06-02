0804817374c21a9c706f4b3edc97724beccfa12b
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-02 00:09:22 -0600

    Option to show amount of money for all items

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -26,6 +26,7 @@ const CartContainer = () => {
       <footer>
         <hr />
         <div className="cart-total">
+        <h4>total <span>${total}</span></h4>
         </div>
       </footer>
     </section>
