1ebbb4512d2e21ec57de92548b66d4addae10735
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-01 23:54:44 -0600

    Add empty option

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -7,6 +7,7 @@ const CartContainer = () => {
     return <section className="cart">
         <header>
             <h2>your bag</h2>
+            <h4 className="empty-cart">is currently empty</h4>
         </header>
     </section>;
   }
