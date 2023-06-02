96cb648d7342bd17d2d430d99ee76ff52f13d602
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-02 00:07:33 -0600

    map over each item

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -4,17 +4,26 @@ import { useSelector } from 'react-redux';
 const CartContainer = () => {
   const { cartItems, total, amount } = useSelector((store) => store.cart);
   if (amount < 1) {
-    return <section className="cart">
+    return (
+      <section className="cart">
         <header>
-            <h2>your bag</h2>
-            <h4 className="empty-cart">is currently empty</h4>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
         </header>
-    </section>;
+      </section>
+    );
   }
-  return <section className="cart">
-        <header>
-            <h2>your bag</h2>
-        </header>
-  </section>;
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => { 
+            return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
 };
 export default CartContainer;
