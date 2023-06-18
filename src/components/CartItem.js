05fb3f4e3e2c8f0497199bf6ba2ceea720517862
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-17 23:57:06 -0600

    Add price

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -6,6 +6,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
       </div>
     </article>
   );
