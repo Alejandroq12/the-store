ad37c2591fdfe96dc53899d75adb61563270a657
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-17 23:56:11 -0600

    Add title

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,6 +4,9 @@ const CartItem = ({ id, img, title, price, amount }) => {
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+      </div>
     </article>
   );
 };
