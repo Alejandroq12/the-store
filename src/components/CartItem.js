7c677d159ff6646f9f015833bcddf5f8cda462a4
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-25 21:25:21 -0600

    Set useDispatch

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,6 +3,7 @@ import { removeItem } from '../features/cart/cartSlice';
 import {  useDispatch } from 'react-redux';
 
 const CartItem = ({ id, img, title, price, amount }) => {
+  const dispatch = useDispatch();
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
