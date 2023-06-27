f3e27303323984cfde9e73cda05055163fa89041
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-27 14:14:29 -0600

    Add logic to decrease items

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -22,12 +22,16 @@ const cartSlice = createSlice({
     increase: (state, {payload}) => {
       const cartItem = state.cartItems.find((item) => item.id === payload.id)
       cartItem.amount = cartItem.amount + 1;
-    }
+    },
+    decrease: (state, {payload}) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
 // console.log(cartSlice);
 
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
