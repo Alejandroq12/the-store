4b8a0f225982bbd91af2abf0fc71b31146408e35
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-27 14:26:00 -0600

    Properly indent code

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,6 @@
 import { ChevronDown, ChevronUp } from '../icons';
-import { removeItem } from '../features/cart/cartSlice';
-import {  useDispatch } from 'react-redux';
-import { increase, decrease } from '../features/cart/cartSlice';
+import { removeItem, increase, decrease } from '../features/cart/cartSlice';
+import { useDispatch } from 'react-redux';
 
 const CartItem = ({ id, img, title, price, amount }) => {
   const dispatch = useDispatch();
@@ -11,14 +10,22 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">${price}</h4>
-        <button className="remove-btn" onClick={()=> {
-          dispatch(removeItem(id));
-        }}>Remove</button>
+        <button
+          className="remove-btn"
+          onClick={() => {
+            dispatch(removeItem(id));
+          }}
+        >
+          Remove
+        </button>
       </div>
       <div>
-        <button className="amount-btn" onClick={() => {
-          dispatch(increase({id}))
-        } }>
+        <button
+          className="amount-btn"
+          onClick={() => {
+            dispatch(increase({ id }));
+          }}
+        >
           <ChevronUp />
         </button>
         <p className="amount">{amount}</p>
