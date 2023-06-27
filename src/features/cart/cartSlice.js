b835979afff6bd54f6027ddef2c07af0acd02b94
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-27 14:46:59 -0600

    Add logic to calculate totals

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -27,11 +27,21 @@ const cartSlice = createSlice({
       const cartItem = state.cartItems.find((item) => item.id === payload.id)
       cartItem.amount = cartItem.amount - 1;
     },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item)=>{
+        amount += item.amount;
+        total += item.amount * item.price;
+      })
+      state.amount = amount;
+      state.total = total;
+    }
   },
 });
 
 // console.log(cartSlice);
 
-export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions;
 
 export default cartSlice.reducer;
