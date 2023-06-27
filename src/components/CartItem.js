d7f42ddedc97ac8b32d1b5558d5411fe1d97c253
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-27 14:33:24 -0600

    Add if statement in order to remove item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -32,6 +32,10 @@ const CartItem = ({ id, img, title, price, amount }) => {
         <button
           className="amount-btn"
           onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
             dispatch(decrease({ id }));
           }}
         >
