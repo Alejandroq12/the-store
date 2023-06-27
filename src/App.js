ca851eedb00addf9e2bd7ad87b6f4ab3b53b6bfe
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-27 15:11:40 -0600

    Add functionality to calculate total amount of items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+import { useEffect } from 'react';
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
   return (
     <main>
       <Navbar />
