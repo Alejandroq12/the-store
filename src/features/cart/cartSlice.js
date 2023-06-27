e4a0731313f0746f8a1600557287d8dc6e8023c6
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-27 14:11:51 -0600

    Add logic to increase items

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -20,7 +20,8 @@ const cartSlice = createSlice({
       state.cartItems = state.cartItems.filter((item)=>item.id !== itemId);
     },
     increase: (state, {payload}) => {
-      
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
+      cartItem.amount = cartItem.amount + 1;
     }
   },
 });
